eedd42acbf3e88ad4a22ae9ac4b2ac343e229b3c
Akamanouche <akamanouche@gmail.com>
2018-09-07 17:06:53 +0200

    Map to Set via Strams (with transformation)

---
+++ java8-rebooted/src/test/java/org/anaolabs/java8/streams/newuse/set/MapToSet.java
@@ -0,0 +1,53 @@
+package org.anaolabs.java8.streams.newuse.set;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author T328469
+ * @formatter:off
+ */
+public class MapToSet {
+	
+	Map<String, String> map = new HashMap<>();
+	
+	@Before
+	public void setup() {
+		map.put( "AF1", "Air France" );
+		map.put( "AF2", "Air France" );
+		map.put( "KL", "Dutch KLM" );
+		map.put( "DL", "Delta Airline" );
+	}
+	
+	/*
+	 * Ici on se contente de récupérer le Set depuis la Map
+	 */
+	@Test
+	public void testGetSetFromMapAsIs() {
+	
+		map.values()
+			.stream()
+			.collect( Collectors.toSet() )
+			.forEach( v -> System.out.println( "\t- "+v ) );
+	}
+	
+	/*
+	 * Ici on applique un petit traitement à chaque élément de la Map avant de récupérer le Set
+	 */
+	@Test
+	public void testGetSetFromMapWithTransformation() {
+	
+		map.values()
+			.stream()
+			.map( v -> v + "-enriched")
+			.collect( Collectors.toSet() )
+			.forEach( v -> System.out.println( "\t- "+v ) );
+	}
+
+}
